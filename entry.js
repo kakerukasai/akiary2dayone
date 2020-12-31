984fb247fbaf175ad5cae1b4fd50096f04981733
Kakeru Kasai <r@eumo.jp>
2020-12-31 09:04:43 +0900

    Consider spaces at the beginning of paragraph

--- entry.js
+++ entry.js
@@ -3,9 +3,11 @@ const sanitizeHtml = require('sanitize-html')
 module.exports = class Entry {
   exportMarkdown () {
     const formattedBody = sanitizeHtml(this.body.replace(/\n/g, ''), {
-            allowedTags: ['br'], // remove all tags and return text content only
-            allowedAttributes: {}, // remove all tags and return text content only
+            allowedTags: ['br', 'b', 'strong'],
+            allowedAttributes: {},
           }).replace(/<br \/>/g, '\n')
+            .replace(/\n　/g, '\n&#8203;　')
+            .replace(/^　/g, '\n&#8203;　')
 
     return this.titleDate + ' ' + this.title + '\n\n' + formattedBody
   }
